9e4f3ccc81bbb1356d2a2d41c6db05b29bda9555
Nikhil Varma <59138968+nikhil7127@users.noreply.github.com>
2021-02-01 12:15:36 +0530

    Update server.py

--- tic tac toe/server.py
+++ tic tac toe/server.py
@@ -4,23 +4,36 @@ import pickle as p
 class Server:
     port = 8000
     IP = socket.gethostbyname(socket.gethostname())
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server = socket.socket()
     server.bind((IP,port))
+    connectionList = []
+
+    def sendBack(self,msg):
+        msg = p.dumps(msg)
+        length = len(msg)
+        temp = p.dumps(str(length)) + b' '*(64-length)
+        for connect,_ in self.connectionList:
+            connect.send(temp)
+            connect.send(msg)
 
     def connect(self,conn,addr):
         connection = True
         print(f"[CONNECTED] {addr}")
+        self.connectionList.append([conn,addr])
         while connection:
             try:
                 let = p.loads(conn.recv(64))
                 if let:
                     message = p.loads(conn.recv(int(let)))
                     if(message=="exit"):
+                        self.sendBack(message)
                         connection = False
                     else: 
                         print(message)
+                        self.sendBack(message)
             except:
                 pass
+        self.connectionList.remove([conn,addr])
         print(f"[DISCONNECTED] {addr}")
 
     def start(self):
