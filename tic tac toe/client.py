e02a4ea15f8831d32e5e90772c743842437d0323
Nikhil Varma <59138968+nikhil7127@users.noreply.github.com>
2021-02-01 12:15:04 +0530

    Update client.py

--- tic tac toe/client.py
+++ tic tac toe/client.py
@@ -1,6 +1,6 @@
 import socket
 import pickle as p 
-
+import threading
 class Client:
     client = socket.socket()
     ip = socket.gethostbyname(socket.gethostname())
@@ -13,10 +13,30 @@ class Client:
         temp = p.dumps(str(temp)) + b" " * (64 - temp)
         self.client.send(temp)
         self.client.send(message)
+    
+    def receive(self):
+        while(1):
+            try:
+                msg = p.loads(self.client.recv(64))
+                if(msg):
+                    msg = p.loads(self.client.recv(int(msg)))
+                    if(msg=="exit"):
+                        break
+                    else:
+                        print((msg))
+
+            except:
+                pass
 
 copy = Client()
-while(1):
-    inp = input()
-    copy.sendMessage(inp)
-    if(inp == "exit"):
-        break
+def sendMsg():
+    while(1):
+        inp = input()
+        copy.sendMessage(inp)
+        if(inp == "exit"):
+            break
+
+recving = threading.Thread(target=copy.receive)
+recving.start()
+send = threading.Thread(target=sendMsg)
+send.start()
